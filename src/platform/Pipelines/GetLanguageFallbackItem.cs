deb5f9ec3723055c4b4df4c900d39de01bda383a
Alexander Naumchenkov <alexander.naumchenkov@sitecore.com>
2023-05-03 15:57:30 +0300

    Test commit.

---
+++ src/platform/Pipelines/GetLanguageFallbackItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Demo.Edge.Website.Pipelines
+{
+    public class GetLanguageFallbackItem
+    {
+    }
+}
